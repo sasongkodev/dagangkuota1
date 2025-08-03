cea7fdffe7ee77df9d1ae171b600fdd9b9f95acf
Wahyu Puji Sasongko <85165620+sasongkodev@users.noreply.github.com>
2025-08-03 17:50:24 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite"; // Impor plugin Tailwind CSS
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // Tambahkan di sini
+  ],
+});
